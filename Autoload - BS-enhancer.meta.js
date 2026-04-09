beef6c5f91cd8e22cf8300e4787762bf7869bebb
FamousZsta5rs <mxx14txx39s@gmail.com>
2026-04-09 12:20:39 +0200

    Create metadata file for BS enhancer userscript
    Add metadata for the Autoload - BS enhancer userscript.

---
+++ Autoload - BS-enhancer.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name           Autoload - BS enhancer
+// @namespace      https://github.com/FamousZsta5rs
+// @version        16.6
+// @description    Metadata file for updates
+// @match          https://bs.to/*
+// ==/UserScript==
