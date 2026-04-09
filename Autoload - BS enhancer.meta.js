ed469a9ff009bf46b1fe71002d52aa41884d59a1
FamousZsta5rs <mxx14txx39s@gmail.com>
2026-04-09 14:28:10 +0200

    Update Autoload - BS enhancer.meta.js

--- Autoload - BS enhancer.meta.js
+++ Autoload - BS enhancer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name           Autoload - BS enhancer
+// @name           Autoload - BS enhancer (modified version of BETSLIX by xtrars)
 // @namespace      https://github.com/FamousZsta5rs
-// @version        16.8
+// @version        17.0
 // @downloadURL    https://raw.githubusercontent.com/FamousZsta5rs/Autoload-BS-enhancer/main/Autoload%20-%20BS%20enhancer.user.js
 // @updateURL      https://raw.githubusercontent.com/FamousZsta5rs/Autoload-BS-enhancer/main/Autoload%20-%20BS%20enhancer.meta.js
 // ==/UserScript==
