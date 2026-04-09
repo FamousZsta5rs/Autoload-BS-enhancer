7a318a416292faaa6b4aa2b43cf3b711778e6887
FamousZsta5rs <mxx14txx39s@gmail.com>
2026-04-09 14:06:46 +0200

    Bump version from 16.7 to 16.8

--- Autoload - BS enhancer.meta.js
+++ Autoload - BS enhancer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Autoload - BS enhancer
 // @namespace      https://github.com/FamousZsta5rs
-// @version        16.7
+// @version        16.8
 // @downloadURL    https://raw.githubusercontent.com/FamousZsta5rs/Autoload-BS-enhancer/main/Autoload%20-%20BS%20enhancer.user.js
 // @updateURL      https://raw.githubusercontent.com/FamousZsta5rs/Autoload-BS-enhancer/main/Autoload%20-%20BS%20enhancer.meta.js
 // ==/UserScript==
