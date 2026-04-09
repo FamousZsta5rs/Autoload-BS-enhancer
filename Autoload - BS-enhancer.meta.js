23a0c8a4850a1df51e1c7e4a857371bd2cdc6fda
FamousZsta5rs <mxx14txx39s@gmail.com>
2026-04-09 12:59:10 +0200

    Update download and update URLs in metadata

---
+++ Autoload - BS-enhancer.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name           Autoload - BS enhancer
+// @namespace      https://github.com/FamousZsta5rs
+// @version        16.6
+// @downloadURL    https://raw.githubusercontent.com/FamousZsta5rs/Autoload-BS-enhancer/refs/heads/main/Autoload%20-%20BS-enhancer.user.js
+// @updateURL      https://raw.githubusercontent.com/FamousZsta5rs/Autoload-BS-enhancer/refs/heads/main/Autoload%20-%20BS-enhancer.meta.js
+// ==/UserScript==
